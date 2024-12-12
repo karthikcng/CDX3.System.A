9e861dcae2eeaebe3546b6a69cec0683cdf90eee
karthikcng <karthikcng1995@gmail.com>
2024-12-12 03:41:18 -0800

    Add project files.

---
+++ CDX3.System.A/Base.cs
@@ -0,0 +1,15 @@
+﻿namespace CDX3.System.A
+{
+    public class Base
+    {
+        public int user_id { get; set; }
+        public int tenant_code { get; set; }
+        public string system_code { get; set; }
+        public Base()
+        {
+            user_id = 0;
+            tenant_code = 0;
+            system_code = "";
+        }
+    }
+}
---
+++ CDX3.System.A/Controllers/AccessController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace CDX3.System.A.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccessController : ControllerBase
+    {     
+
+        [HttpPost]
+        [Route("Feature1")]
+        public JsonResult Feature1(Base ip)
+        {
+            var actionName = MethodBase.GetCurrentMethod().Name; 
+            
+            var op = new
+            {                
+                Action = actionName,                
+            };
+
+            return new JsonResult(op);
+        }
+
+        [HttpPost]
+        [Route("Feature2")]
+        public JsonResult Feature2(Base ip)
+        {
+            var actionName = MethodBase.GetCurrentMethod().Name;
+
+            var op = new
+            {
+                Action = actionName,
+            };
+
+            return new JsonResult(op);
+        }
+    }
+}
